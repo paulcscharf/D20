b48e899cadbdab18f43a63c77dd74cb7d7b1ceb6
amulware <amul@amulware.net>
2016-12-19 20:21:30 +0100

    :sparkles: Add static dice literals and extensions

---
+++ D20/rollables/Extensions.cs
@@ -0,0 +1,15 @@
+﻿namespace D20
+{
+    public static class Extensions
+    {
+        public static Dice D2(this int count) => Dice.D2(count);
+        public static Dice D4(this int count) => Dice.D4(count);
+        public static Dice D6(this int count) => Dice.D6(count);
+        public static Dice D8(this int count) => Dice.D8(count);
+        public static Dice D10(this int count) => Dice.D10(count);
+        public static Dice D12(this int count) => Dice.D12(count);
+        public static Dice D20(this int count) => Dice.D20(count);
+
+        public static Dice D(this int count, int value) => new Dice(count, value);
+    }
+}
---
+++ D20/rollables/Statics.cs
@@ -0,0 +1,22 @@
+﻿namespace D20
+{
+    public static class Statics
+    {
+        public static readonly Die D2 = Die.D2;
+        public static readonly Die D4 = Die.D4;
+        public static readonly Die D6 = Die.D6;
+        public static readonly Die D8 = Die.D8;
+        public static readonly Die D10 = Die.D10;
+        public static readonly Die D12 = Die.D12;
+        public static readonly Die D20 = Die.D20;
+
+        public static Dice D2s(int count) => new Dice(count, 2);
+        public static Dice D4s(int count) => new Dice(count, 4);
+        public static Dice D6s(int count) => new Dice(count, 6);
+        public static Dice D8s(int count) => new Dice(count, 8);
+        public static Dice D10s(int count) => new Dice(count, 10);
+        public static Dice D12s(int count) => new Dice(count, 12);
+        public static Dice D20s(int count) => new Dice(count, 20);
+
+    }
+}
